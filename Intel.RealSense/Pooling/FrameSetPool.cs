882b18c36a2eb7e07f9cf53e32f56bb6556de946
Maximilian Krüger <Maximilian.Krueger@airsphere.de>
2018-12-17 16:30:13 +0100

    Add FrameSetPool

---
+++ Intel.RealSense/Pooling/FrameSetPool.cs
@@ -0,0 +1,25 @@
+﻿using Intel.RealSense.Pooling;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Intel.RealSense
+{
+    public class FrameSetPool : Pool<FrameSet>
+    {
+        public async Task<FrameSet> Next(IntPtr ptr, CancellationToken cancellationToken)
+        {
+            var result = await Next(cancellationToken);
+
+            if (result == null)
+                result = new FrameSet(ptr);
+
+            return result;
+        }
+    }
+}

6a1cfc70b517eb45786b166993233873ce0fa9c7
Maximilian Krüger <Maximilian.Krueger@airsphere.de>
2018-12-17 15:24:55 +0100

    Add Poolinterfaces
    * Add normal pool and poolElement
    * Add generic pool and poolElement
    * Add normal async pool and async poolElement
    * Add generic async pool and async poolElement

---
+++ Intel.RealSense/Pooling/IPool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Intel.RealSense.Pooling
+{
+    public interface IPool
+    {
+        void OnError(Exception exception);
+        void OnRelease(IPoolElement poolElement);
+        void OnFinalize(IPoolElement poolElement);
+    }
+    public interface IPool<T>
+    {
+        void OnError(Exception exception);
+        void OnRelease(IPoolElement<T> poolElement);
+        void OnFinalize(IPoolElement<T> poolElement);
+    }
+    public interface IPoolAsync
+    {
+        Task OnError(Exception exception, CancellationToken cancellationToken);
+        Task OnRelease(IPoolElement poolElement, CancellationToken cancellationToken);
+        Task OnFinalize(IPoolElement poolElement, CancellationToken cancellationToken);
+    }
+    public interface IPoolAsync<T>
+    {
+        Task OnError(Exception exception, CancellationToken cancellationToken);
+        Task OnRelease(IPoolElement<T> poolElement, CancellationToken cancellationToken);
+        Task OnFinalize(IPoolElement<T> poolElement, CancellationToken cancellationToken);
+    }
+}
---
+++ Intel.RealSense/Pooling/IPoolElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Intel.RealSense.Pooling
+{
+    public interface IPoolElement
+    {
+        void Pool(IPool pool);
+        void Release();
+    }
+    public interface IPoolElement<T>
+    {
+        void Pool(IPool<T> pool);
+        void Release();
+    }
+    public interface IPoolElementAsync
+    {
+        Task Pool(IPoolAsync pool, CancellationToken cancellationToken);
+        Task Release(CancellationToken cancellationToken);
+    }
+    public interface IPoolElementAsync<T>
+    {
+        Task Pool(IPoolAsync<T> pool, CancellationToken cancellationToken);
+        Task Release(CancellationToken cancellationToken);
+    }
+}
